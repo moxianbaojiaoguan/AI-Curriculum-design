226de1f3602b08c2cd13a264fb00ee97c0e189c8
moxianbaojiaoguan <106585500+moxianbaojiaoguan@users.noreply.github.com>
2024-01-11 18:07:12 +0800

    Add files via upload

---
+++ scripts/dataset_setup_for_yolov5.sh
@@ -0,0 +1,3 @@
+bash scripts/download_road2020.sh
+bash scripts/prepare_test.sh
+python scripts/xml2yolo.py
---
+++ scripts/prepare_test.sh
@@ -0,0 +1,4 @@
+cd datasets/road2020
+echo "move test images to a flat directory structure required by yolo..."
+python move_test_iamges.py
+cd -
